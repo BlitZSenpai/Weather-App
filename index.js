36f5feff78bbf62bf4868064f4c8e2f2f9621d04
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-12 17:49:41 +0530

    fetch data

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const apiKey = "8f64d20bf029999aaae622dec4a88818";
+const cityName = document.getElementById("text-input");
+const weatherData = document.getElementById("weatherData");
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  const cityValue = cityName.value;
+  event.preventDefault();
+  getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`
+    );
+    if (!response.ok) {
+      throw new Error("404 error");
+    }
+    const data = await response.json();
+    console.log(data);
+    const temp = Math.round(data.main.temp);
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+  } catch (error) {}
+}
