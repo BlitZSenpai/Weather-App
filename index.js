490001c940467e2646daf3b65ac2e6db2365edc9
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-12 18:36:10 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
-const apiKey = "8f64d20bf029999aaae622dec4a88818";
+const apiKey = "9c40b7b4fe68bbf8e50b8bc712010a8a";
 const cityName = document.getElementById("text-input");
 const weatherData = document.getElementById("weatherData");
 
 document.querySelector("form").addEventListener("submit", (event) => {
-  const cityValue = cityName.value;
   event.preventDefault();
+  const cityValue = cityName.value;
   getWeatherData(cityValue);
 });
 
 async function getWeatherData(cityValue) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`
     );
     if (!response.ok) {
       throw new Error("404 error");
     }
     const data = await response.json();
-    console.log(data);
     const temp = Math.round(data.main.temp);
     const description = data.weather[0].description;
     const icon = data.weather[0].icon;
-  } catch (error) {}
+
+    const details = [
+      `Feels like : ${Math.round(data.main.feels_like)}`,
+      `Humidity : ${data.main.humidity}%`,
+      `Wind Speed : ${data.wind.speed} m/s`,
+    ];
+
+    weatherData.querySelector(
+      ".icon"
+    ).innerHTML = `<img src=http://openweathermap.org/img/wn/${icon}.png alt="Weather Icon" >`;
+
+    weatherData.querySelector(".temperature").textContent = `${temp}°C`;
+
+    weatherData.querySelector(".description").textContent = description;
+
+    weatherData.querySelector(".details").innerHTML = details
+      .map((detail) => `<div>${detail}</div>`)
+      .join("");
+  } catch (error) {
+    console.log(error);
+  }
 }
